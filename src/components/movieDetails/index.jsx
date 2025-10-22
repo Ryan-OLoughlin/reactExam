86f8e290734d6e27ee4c74ab06382da150921d7f
Ryan-OLoughlin <ryanol2010@hotmail.com>
2025-10-22 13:37:41 +0100

    Added Original Language attribute

--- src/components/movieDetails/index.jsx
+++ src/components/movieDetails/index.jsx
@@ -9,7 +9,7 @@ import Fab from "@mui/material/Fab";
 import Typography from "@mui/material/Typography";
 import Drawer from "@mui/material/Drawer";
 import MovieReviews from "../movieReviews"
-
+import LanguageIcon from '@mui/icons-material/Language';
 
 const root = {
   display: "flex",
@@ -57,6 +57,10 @@ const MovieDetails = ({ movie }) => {  // Don't miss this!
         <Chip
           icon={<StarRate />}
           label={`${movie.vote_average} (${movie.vote_count})`}
+        />
+        <Chip
+          icon={<LanguageIcon />}
+          label={`Original Language ${movie.original_language}`}
         />
         <Chip label={`Released: ${movie.release_date}`} />
       </Paper>
